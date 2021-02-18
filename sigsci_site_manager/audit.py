ac164d405ff7d7417030913b47a639ebfcf3e710
Joey Leake <61753229+joeyleake@users.noreply.github.com>
2021-02-18 13:00:37 -0500

    add functions to audit a site
    add functions to audit a site's compliance based on the alerts contained in a specified "baseline" site

---
+++ sigsci_site_manager/audit.py
@@ -0,0 +1,66 @@
+import os
+import json
+import re
+from sigsci_site_manager import api
+
+if 'SIGSCI_EMAIL' in os.environ:
+    username = os.environ['SIGSCI_EMAIL']
+if 'SIGSCI_PASSWORD' in os.environ:
+    password = os.environ['SIGSCI_PASSWORD']
+if 'SIGSCI_API_TOKEN' in os.environ:
+    token = os.environ['SIGSCI_API_TOKEN']
+if 'SIGSCI_CORP' in os.environ:
+    corp = os.environ['SIGSCI_CORP']
+
+my_api = api.init_api(username, None, token, corp)
+
+def get_site_custom_alerts(site, active_only=False):
+    alerts = []
+    my_api.site = site
+    for alert in my_api.get_custom_alerts()['data']:
+        if alert['type'] == 'siteAlert':
+            if (active_only and alert['enabled']) or (not active_only):
+                alerts.append(alert)
+    return alerts
+
+def create_audit_dict(alerts):
+    ''' Create a baseline compliance reference. Any (x.y.z) values contained in
+        an alert's longName value will be interpreted as indicating that alert
+        confers compliance with the referenced section of the baseline.
+        ie - an alert named "(1.2) FooBar" will be assumed to grant compliance
+        with baseline section 1.2
+    '''
+    audit_ref = {}
+    for alert in alerts:
+        matches = re.findall("\(((\d+\.)?(\d+\.)?(\*|\d+))\)", alert['longName'])
+        for match in matches:
+            try:
+                audit_ref[alert['tagName']].add(match[0])
+            except KeyError:
+                audit_ref[alert['tagName']] = set([match[0]])
+    return {x: tuple(audit_ref[x]) for x in audit_ref}
+
+def save_baseline_json(site):
+    with open('audit_baseline.json', 'w') as f:
+        json.dump(create_audit_dict(get_site_custom_alerts(site)), f)
+
+def audit_site(site, baseline=None):
+    compliance_ref = {'OTHER': []}
+    if baseline is None:
+        if os.path.isfile('audit_baseline.json'):
+            baseline = json.load(open('audit_baseline.json'))
+        else:
+            raise Exception("A baseline is required.")
+    else:
+        baseline = create_audit_dict(get_site_custom_alerts(baseline))
+    for sections in baseline.values():
+        for section in sections:
+            compliance_ref[section] = []
+    audit_site_alerts = get_site_custom_alerts(site)
+    for alert in audit_site_alerts:
+        try:
+            for section in baseline[alert['tagName']]:
+                compliance_ref[section].append((alert['tagName'], alert['longName']))
+        except KeyError:
+            compliance_ref['OTHER'].append((alert['tagName'], alert['longName']))
+    return compliance_ref
